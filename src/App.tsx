83d40688d02b6f394f96b8f88b25967078278bbf
Antonio Emerson <emerson.nessa@gmail.com>
2022-08-05 03:37:01 -0300

    menu hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import { Nav } from "./components/Nav";
+import imgHero from "./images/image-hero-mobile.png"
 
 function App() {
   return (
   <>
   <Nav />
+<div className="">
+<img src={imgHero}></img>
+
+<div className="container justify-center text-center mt-12 flex-col ">
+<p className="text-4xl font-bold mb-6">Make remote work</p>
+<h3 className="w-screen text-xl leading-8 mb-4">Get your team in sync, no matter your location. Streamline processes. create team rituals, and watch productivity soar.</h3>
+</div>
+</div>
+<div className="flex items-center justify-center">
+  <button className="border-2 border-black px-6 py-2 rounded-xl bg-black text-white">Lear more</button>
+</div>
   </>
   )
 }
--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -4,28 +4,28 @@ export function Nav() {
   const [isNavOpen, setIsNavOpen] = useState(false); // initiate isNavOpen state with false
 
   return (
-    <div className="container flex items-center justify-between border-b border-gray-400 py-8">
+    <div className="container flex items-center justify-between border-gray-300 mt-4 mb-6">
       <a href="/">
 <h1 className="text-4xl font-bold ml-4">snap</h1>
       </a>
       <nav>
         <section className="MOBILE-MENU flex lg:hidden mr-4">
           <div
-            className="HAMBURGER-ICON space-y-2"
+            className="HAMBURGER-ICON space-y-1.5"
             onClick={() => setIsNavOpen((prev) => !prev)}
           >
-            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
-            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
-            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+            <span className="block h-0.5 w-8 animate-pulse bg-gray-700"></span>
+            <span className="block h-0.5 w-8 animate-pulse bg-gray-700"></span>
+            <span className="block h-0.5 w-8 animate-pulse bg-gray-700"></span>
           </div>
 
           <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}> 
             <div
-              className="CROSS-ICON absolute top-0 right-0 px-8 py-8"
+              className="CROSS-ICON absolute top-0 right-0 px-4 py-4"
               onClick={() => setIsNavOpen(false)} 
             >
               <svg
-                className="h-8 w-8 text-gray-600"
+                className="h-10 w-10 text-gray-600"
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
@@ -37,21 +37,30 @@ export function Nav() {
                 <line x1="6" y1="6" x2="18" y2="18" />
               </svg>
             </div>
-            <ul className="MENU-LINK-MOBILE-OPEN flex flex-col items-center justify-between min-h-[250px]">
-              <li className="border-b border-gray-400 my-8 uppercase">
+            <ul className="MENU-LINK-MOBILE-OPEN flex flex-col  ">
+              <li className=" border-gray-400 my-2 mx-2 ">
                 <a href="/about">Features</a>
               </li>
-              <li className="border-b border-gray-400 my-8 uppercase">
+              <li className=" border-gray-400 my-2 mx-2 ">
                 <a href="/portfolio">Company</a>
               </li>
-              <li className="border-b border-gray-400 my-8 uppercase">
+              <li className=" border-gray-400 my-2 mx-2 ">
                 <a href="/contact">Careers</a>
               </li>
-              <li className="border-b border-gray-400 my-8 uppercase">
+              <li className=" border-gray-400 my-2 mx-2 mb-10">
                 <a href="/contact">About</a>
               </li>
+              <div className="flex flex-col items-center">
+            <span className=" border-gray-400 mx-8 mb-4">
+                <a href="/contact">Login</a>
+              </span>
+              <button className="border-2 border-black px-16 py-2 rounded-xl">Register</button>
+            </div>
             </ul>
+    
+    
           </div>
+
         </section>
 
         <ul className="DESKTOP-MENU hidden space-x-8 lg:flex">
@@ -77,16 +86,17 @@ export function Nav() {
       .showMenuNav {
         display: block;
         position: absolute;
-        width: 100%;
-        height: 100vh;
+        width: 90%;
+        height: 70vh;
         top: 0;
         left: 0;
-        background: white;
+        background: gray;
         z-index: 10;
         display: flex;
         flex-direction: column;
         justify-content: space-evenly;
         align-items: center;
+
       }
     `}</style>
     </div>
